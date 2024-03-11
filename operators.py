9bfc21650888c4c92cb95da8f2d091b84307ad7b
Mohammad Rasoul Azizi <azizi.mr1377@gmail.com>
2024-03-11 10:07:01 +0330

    first file added

---
+++ operators.py
@@ -0,0 +1,87 @@
+# Part One:
+# show output:
+# for showing the output in terminal you must to use the "print(...)" command
+# begin Code Part one:
+
+# print("Programming", "Math")
+# print("Ali", "kermanshah", "Iran", sep=' - ')
+# print("python", "C++", "Java", "Php", end=' the end line \n')
+# print("python", "C++", "Java", "Php", end=' :) ', sep=' -> ')
+
+# End Code Part one.
+# ------------------------------------------------------------------------------
+
+
+# Part Two
+# getting data from user:
+# for getting data from user you must use the "Input(promt='')"
+# begin Code Part Two:
+
+# temp_variable = input()
+# temp_variable_2 = input("Enter the number: ")
+
+# End Code part Two.
+# ------------------------------------------------------------------------------
+
+# Part Three
+# math operators:
+# Begin Code Part one
+
+# num1 = 15
+# num2 = 4
+
+# print("sum of num1 and num2:", num1 + num2)
+# print("minus of num1 and num2:", num1 - num2)
+# print("mul of num1 and num2:", num1 * num2)
+# print("divide of num1 and num2:", num1 / num2)
+# print("mode of num1 and num2:", num1 % num2)
+# print("pow of num1 and num2:", num1 ** num2)
+# print("int division of num1 and num2:", num1 // num2)
+
+# End Code part Three. 
+# -------------------------------------------
+
+
+
+
+# Naming rules:
+# The first rule: the length of the variable name is not important, but the variable name must be meaningful and related to its work
+# The second rule: There should be no space between the words of variable nouns
+# The third rule: A noun can only contain uppercase and lowercase English letters, numbers and underscores
+# Fourth rule: If the name of the variable is two or more words, we use the underline sign
+# The fifth rule: Bringing the first number of the name of the variable is wrong
+# The sixth rule: Using keywords for variable names is wrong
+
+
+# نمونه هایی از اسامی صحیح
+# name = 'Bardia'
+# Iran1403 = 20
+# name_family = 'Bardia Tabesh'
+
+# نمونه های از اسامی غلط
+# 1403Iran = 20
+# name family = 'bardia Tabesh'
+# digi;kala = 13.4
+# print = 'rrrr'
+
+# آموزش در مورد دستورات
+# help("keywords")
+# help(input)
+
+
+# خواندن طول رشته 
+# name = "Bardia"
+# print( len(name) )
+
+
+# عملیات ریاضی روی رشته ها
+# my_str1 = 'abc'
+# my_str2 = 'xyz'
+
+# temp = my_str2 + my_str1
+# print(temp)
+
+# temp2 = my_str1 * 5
+# print(temp2)
+
+
