b10afb23d036a7d9a09608355e79ba9f3364fab3
Mohammad Rasoul Azizi <azizi.mr1377@gmail.com>
2024-03-11 23:20:06 +0330

    add condition file to repo

---
+++ conditions session.py
@@ -0,0 +1,86 @@
+# Conditions Session
+
+# part one:
+# Comparative operations
+# Start Code:
+
+# num1 = 12
+# num2 = 45
+
+# print( f'num1 == num2 : {num1 == num2}')
+# print( f'num1 != num2 : {num1 != num2}')
+# print( f'num1 <= num2 : {num1 <= num2}')
+# print( f'num1 >= num2 : {num1 >= num2}')
+# print( f'num1 > num2 : {num1 > num2}')
+# print( f'num1 < num2 : {num1 < num2}')
+
+# print('-'*30)
+
+# mystr1 = "Bardia"
+# mystr2 = "Mohammad"
+# print( f'mystr1 == mystr2 : {mystr1 == mystr2}')
+# print( f'mystr1 != mystr2 : {mystr1 != mystr2}')
+# print( f'mystr1 == mystr2 : {type(mystr1) == type(mystr2)}')
+
+
+# End Code
+# ----------------------------------
+# part two:
+# if conditions
+# Start Code:
+
+
+# if 2 == 3: #False
+#     print(True)
+#     print('Condition is True')
+# elif 2 > 3: #False
+#     print("2 < 3")
+# else:
+#     print(False)
+# print("End of condition")
+
+
+# End Code
+# -----------------------------------
+# part three:
+# note1
+# Start Code:
+
+# num1 = int( input("Enter number1: ") ) #5
+# num2 = int( input("Enter number2: ") ) #3
+
+# if num1 > num2:
+#     print(True)
+# if num1 < num2:
+#     print(True)
+
+# if num1 > num2:
+#     print(True)
+# elif num1 < num2:
+#     print(True)
+
+# End Code
+# -------------------------------------
+# part four:
+# Ternery
+# Start Code:
+
+# print("Hello World") if 2 == 2 else print(False)
+
+## if 2==3:
+##     print("Hello World")
+## else:
+##     print(False)
+
+# End Code
+# -------------------------------------
+# part five:
+# several inputs in one line
+# Start Code
+
+# a, b, c = map(float, input().split())
+# print(a, type(a))
+# print(b, type(b))
+# print(c, type(c))
+
+# End Code
