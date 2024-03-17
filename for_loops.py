310a651a7aa0fc18bbebdfe574455b8c1a534e5c
Mohammad Rasoul Azizi <azizi.mr1377@gmail.com>
2024-03-17 18:38:55 +0330

    add for loop file

---
+++ for_loops.py
@@ -0,0 +1,72 @@
+# County loops
+# part one:
+
+# Beggin Code:
+
+# for i in range(10): # 0 1 2 3 ... 9, i = 0
+#     print(i * 5)
+
+# print("End of Loop")
+
+# print( list( range(10) ) )
+
+# End Code
+# ----------------------------------
+# question one:
+
+# # define a variable to sum all of scores
+# sum_ = 0
+
+# # create a loop for getting scores and sum with sum_
+# for i in range(5):
+
+#     # getting a score
+#     x = float(input(f"Enter score {i+1}: "))
+
+#     # sum of x and sum_
+#     sum_ += x
+
+# # calculate of average of scores
+# ave = sum_ / 5
+
+# # showing the average in ouput
+# print(f'Average = {ave}')
+
+# -----------------------------------
+# question two:
+
+# # getting the number of lessons
+# num_of_lessons = int(input("Enter number of lessons: "))
+
+# # define a variable to sum all of scores
+# sum_ = 0
+
+# # create a loop for getting scores and sum with sum_
+# for i in range(num_of_lessons):
+
+#     # getting a score
+#     x = float(input(f"Enter score {i+1}: "))
+
+#     # sum of x and sum_
+#     sum_ += x
+
+# # calculate of average of scores
+# ave = sum_ / num_of_lessons
+
+# # showing the average in ouput
+# print(f'Average = {ave}')
+
+# -------------------------------
+# print( list( range(20) ) )
+# print( list( range(5, 20) ) )
+# print( list( range(5, 20, 3) ) )
+# print( range(5, 20, 3) ) 
+# ---------------------------
+# for i in range(0, 101, 5):
+#     print(i)
+
+# --------------------------
+# name = "Bardia Tabesh"
+
+# for i in name:
+#     print(i, end=' -> ')
